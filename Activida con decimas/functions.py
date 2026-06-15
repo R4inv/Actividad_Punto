b7c03b9f72cac95d30a61e4a566169df88ad98b9
TSGRE <paztony713@gmail.com>
2026-06-14 22:04:08 -0400

    PR de JhonPaz

--- Activida con decimas/functions.py
+++ Activida con decimas/functions.py
@@ -1,106 +1,155 @@
 import os, time
 def delay():
     time.sleep(1)
+    #################
 def limpiar():
+    ###########
     os.system("cls")
+def tiempo_espera():
+    time.sleep(1)
 
 #.append() para agregar un elemento al final de una lista
 libros = []
 
 def agregar_libro():
-    titulo = input("ingrese titulo: ")
+    limpiar()
+
+
+
+
+
+    while True:
+        titulo = input("ingrese titulo: ")
+        duplicado = False
+        for libro in libros:
+            if libro["titulo"].lower() == titulo.lower():
+                duplicado= True
+
+        if duplicado == True:
+            print("Titulo ya Existente")
+            continue
+        else:
+            break
+
+
+
+
     autor = input("ingrese autor: ")
-    libroplus = {"titulo": titulo, "autor": autor, "estado": "disponible" }
-    if libroplus in libros:
-        print("!No se permiten libros repetidos")
-    else:
-        libros.append(libroplus)
-        print(f"libro: {titulo} agregado")
+    ####################
+    numero_id = len(libros) + 1
+    libroplus = {"numero" : numero_id ,"titulo": titulo, "autor": autor, "estado": "disponible" }
+    #####################
+    libros.append(libroplus)
+    print(f"\nLibro: '{titulo}' agregado!!!\n")
+        
 
 
 def mostrar_libros():
+    limpiar()
+    print("\n   ================ LISTA DE LIBROS ================  \n")
     for libro in libros:
-        print(libro)
-
+         
+         ############
+        if libro['estado'] == "disponible":
+            estado = "Disponible"
+        else:
+            estado = "Prestado"
+        print(f"Libro N°{libro['numero']}\n\nTitulo:  {libro['titulo']}\nAutor:   {libro['autor']}\nEstado:  {estado}")
+        print("\n=============================================================\n")
+        tiempo_espera()
+        ##########
 
 def buscar_libro():
+    limpiar()
     estadolibro = input(" ingrese titulo del libro que busca: ")
     encontrado = False
     #for x in libros:
     for libro in libros:
         if libro["titulo"].lower() == estadolibro.lower():
-            encontrado = True
-            print("libro disponible")
-            print(libro)
+            encontrado = True          
+            print("\nlibro disponible!!\n==================")
+            print(f"Libro N°{libro['numero']} \n\nTitulo:  {libro['titulo']}\nAutor: {libro['autor']}\nEstado: {libro['estado']} \n")##########################################
             break
     if not encontrado:
-        print ("!Libro no disponible")
+        print ("\n!Libro no disponible!!!\n")
 
 
 def prestar_libro ():
-    prestar = input(" ingrese titulo del libro que quiere tomar prestado: ")
+    limpiar()
+    prestar = input(" Ingrese titulo del libro que quiere tomar prestado: ")
     encontrado = False 
     for libro in libros:
         if libro["titulo"].lower() == prestar.lower():
             encontrado = True
             if libro["estado"] == "prestado":
-                print(f"el libro: {libro["titulo"]} ya fue prestado")
+                print(f"\nel libro: '{libro["titulo"]}' ya fue prestado!!!\n")
             else:
                 libro["estado"] = "prestado"
-                print(f"libro: {libro["titulo"]} prestado con exito")
+                print(f"\nlibro: '{libro["titulo"]}' prestado con exito!!!\n")
             break
     if not encontrado:
-        print(f"!El libro {prestar} no esta en el sistema")
+        print(f"\n!El libro '{prestar}' no esta en el sistema!!!\n")
 
 
 def devolver_libro ():
+    limpiar()
     devolver = input(" ingrese titulo del libro que quiere regresar: ")
     encontrado = False
     for libro in libros:
         if libro["titulo"].lower() == devolver.lower():
             encontrado = True
             if libro["estado"] == "disponible":
-                print(f"el libro: {libro["titulo"]} el libro fue devuelto anteriormente")
+                print(f"\nel libro: '{libro["titulo"]}' el libro fue devuelto anteriormente!!!\n")
             elif libro["estado"] == "prestado":
                 libro["estado"] = "disponible"
-                print("libro devuelto exitosamente")
+                print("\nlibro devuelto exitosamente!!!\n")
             break
     if not encontrado:
-        print(f"!El libro {devolver} que desea devolver no esta en el sistema")
+        print(f"\n!El libro '{devolver}' que desea devolver no esta en el sistema!!!\n")
 
 
 def eliminar_libro ():
+    limpiar()
     eliminar = input("ingrese titulo del libro que desea eliminar: ")
     encontrado = False
     for libro in libros:
-        print(f"L1:{libro}")
+        
         if libro["titulo"].lower() == eliminar.lower():
-            print(f"L2:{libro}")
+            
             encontrado = True
             libros.remove(libro)
-            print(f"el libro: {libro["titulo"]} a sido eliminado del sistema")
+            ####################
+            numero_id = 1  
+
+            for libro in libros:
+                libro["numero"] = numero_id 
+                numero_id = numero_id + 1
+                #########################
+            print(f"\nEl libro: '{eliminar}' a sido eliminado del sistema!!!\n")
             break
     if not encontrado:
-        print("!El libro que desea eliminar no esta en el sistema")
+        print("\n!El libro que desea eliminar no esta en el sistema!!!\n")
 
 
 def modificar_libro():
-    modificar = input(" ingrese titulo del libro que desea modificar: ")
+    limpiar()
+    modificar = input(" Ingrese titulo del libro que desea modificar: ")
     encontrado =  False
     for libro in libros:
         if libro["titulo"].lower() == modificar.lower():
             encontrado = True
-            nuevo_titulo = input(" ingrese nuevo titulo: ")
-            nuevo_autor = input(" ingrese nuevo autor: ")
+            nuevo_titulo = input("\n Ingrese nuevo titulo: ")
+            nuevo_autor = input(" Ingrese nuevo autor: ")
             libro["titulo"] = nuevo_titulo
             libro["autor"] = nuevo_autor
-            print("libro modificado con exito")
-        break
+            print("\nLibro modificado con exito!!!\n")
+            break
     if not encontrado:
-        print("!El libro que desea modificar no existe")
+        print("\n!El libro que desea modificar no existe\n")
 
 
 def mostrar_estadisticas():
+    limpiar()
 
     total_libros = len(libros)
     disponibles = 0
@@ -111,10 +160,10 @@ def mostrar_estadisticas():
             disponibles+=1
         elif libro["estado"] == "prestado":
             prestados+=1
-        
-    print(f"· Cantidad total de libros:   {total_libros}")
-    print(f"· Cantidad de libros disponibles: {disponibles}")
-    print(f"· Cantidad de libros prestados:   {prestados}")
+                                                          
+    print(f"· Cantidad total de libros:   {total_libros}\n==================================")
+    print(f"· Libros disponibles: {disponibles}")
+    print(f"· Libros prestados:   {prestados}\n")
 
 
 
